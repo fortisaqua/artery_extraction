5d9ea32b37ecc06b16f4c695e10f081e40f71a18
fortisaqua <1336250263@qq.com>
2018-03-30 11:29:17 +0800

    updated many data extraction methods

--- data_class.py
+++ data_class.py
@@ -6,6 +6,7 @@ import dicom_read
 import cPickle as pickle
 import scipy.io as sio
 import gc
+import sys
 
 class Data:
     def __init__(self,data_dir,number):
@@ -37,20 +38,54 @@ class Data:
         sio.savemat(output_name,{name : self.imgs[name] for name in self.names})
         return output_name
 
+    def output_specific(self,type):
+        root_dir = os.getcwd()
+        self.output_loc = "output_"+type
+        if not os.path.exists("./" + self.output_loc):
+            os.makedirs("./" + self.output_loc)
+        output_dir = root_dir + "/" + self.output_loc
+        output_name = output_dir + "/data_" + str(self.number) + ".mat"
+        output_dict = {}
+        for name,data in self.imgs.items():
+            if "origin" in name:
+                output_dict["original"] = data
+            if type in name:
+                output_dict["mask"] = data
+        sio.savemat(output_name, output_dict)
+        return output_name
 
-root_dir='/opt/Multi-Task-data-process/multi_task_data_train'
-number = 0
-data_meta = dict()
-for patient_dir in os.listdir(root_dir):
-    number += 1
-    data = Data(root_dir+'/'+patient_dir,number)
-    data.load_data()
-    data_meta[number] = data.output()
-    del data
-    gc.collect()
-    print ""
-for num,loc in data_meta.items():
-    print num," ",loc
-pickle_writer = open('./data_meta_multi.pkl', 'wb')
-pickle.dump(data_meta, pickle_writer)
-pickle_writer.close()
+if __name__ == "__main__":
+    root_dir=sys.argv[1]
+    mode = sys.argv[2]
+    data_meta = dict()
+    type = ""
+    output_file = './data_meta_'+mode+'.pkl'
+    if mode == "mask":
+        try:
+            type = sys.argv[3]
+            output_file = './data_meta_'+type+'.pkl'
+        except Exception,e:
+            type = ""
+    if mode == "multi_class":
+        output_file = './data_meta_' + mode + '.pkl'
+    if os.path.isfile(output_file):
+        pickle_reder = open(output_file,'rb')
+        data_meta = pickle.load(pickle_reder)
+        pickle_reder.close()
+    number = len(data_meta)
+    for patient_dir in os.listdir(root_dir):
+        number += 1
+        data = Data(root_dir+'/'+patient_dir,number)
+        data.load_data()
+        if mode == "mask":
+            data_meta[number] = data.output_specific(type)
+        else:
+            data_meta[number] = data.output()
+        del data
+        gc.collect()
+        print ""
+    for num,loc in data_meta.items():
+        print num," ",loc
+    pickle_writer = open(output_file, 'wb')
+    pickle.dump(data_meta, pickle_writer)
+    pickle_writer.close()
