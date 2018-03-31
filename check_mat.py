6d22ca05261f7886aaa2d7c93e9a7139556bc44c
fortisaqua <1336250263@qq.com>
2018-03-31 15:45:05 +0800

    changed check function

--- check_mat.py
+++ check_mat.py
@@ -3,14 +3,15 @@ import scipy.io as sio
 import SimpleITK as ST
 import os
 
-output_root = "./vtks_airway"
-mat_dir = "./output_airway/data1.mat"
+data_type = "multi"
+output_root = "./vtks_"+data_type
+mat_dir = "./output_"+data_type+"/data_1.mat"
 if not os.path.exists(output_root):
     os.mkdir(output_root)
-names = ['original','mask']
 data_sample = sio.loadmat(mat_dir)
 for name,data in data_sample.items():
     try:
+        print type(data_sample[name])
         img = ST.GetImageFromArray(np.transpose(data_sample[name],[2,1,0]))
         ST.WriteImage(img,output_root+ "/" +name+".vtk")
     except Exception,e:
