d44cebef9f4d1437b573a5b1e23468e91f0ddb8a
fortisaqua <1336250263@qq.com>
2018-11-12 19:06:23 +0800

    completedliver-data-set-sepecificed data extraction bash program and data pair outputing python program

--- data_class.py
+++ data_class.py
@@ -21,13 +21,25 @@ class Data:
         ret_array = np.transpose(ret_array, [2, 1, 0])
         return ret_array
 
-    def load_data(self):
+    def load_data(self, typeName="None"):
         self.imgs = dict()
         root_dir = self.dir
-        for sub_dir in os.listdir(root_dir):
-            dicom_dir = root_dir + '/' + sub_dir
-            self.imgs[sub_dir] = self.get_array(dicom_dir)
-            self.names.append(sub_dir)
+        skip_list = ["vtks"]
+        multiClasses = True
+        if typeName == "None":
+            multiClasses = False
+        if multiClasses:
+            for sub_dir in os.listdir(root_dir):
+                if not sub_dir in skip_list:
+                    dicom_dir = root_dir + '/' + sub_dir
+                    self.imgs[sub_dir] = self.get_array(dicom_dir)
+                    self.names.append(sub_dir)
+        else:
+            for sub_dir in os.listdir(root_dir):
+                if sub_dir == typeName:
+                    dicom_dir = root_dir + '/' + sub_dir
+                    self.imgs[sub_dir] = self.get_array(dicom_dir)
+                    self.names.append(sub_dir)
 
     def output(self):
         mask_names=["airway","artery"]
@@ -49,18 +61,18 @@ class Data:
         sio.savemat(output_name,output_dict)
         return output_name
 
-    def output_specific(self, type):
-        root_dir = os.getcwd()
-        self.output_loc = "output_"+type
-        if not os.path.exists("./" + self.output_loc):
-            os.makedirs("./" + self.output_loc)
-        output_dir = root_dir + "/" + self.output_loc
-        output_name = output_dir + "/data_" + str(self.number) + ".mat"
+    def output_specific(self, typeName, output_root):
+        # root_dir = output_root
+        self.output_loc = output_root+"/organized_"+typeName
+        if not os.path.exists(self.output_loc):
+            os.makedirs(self.output_loc)
+        # output_dir = self.output_loc
+        output_name = self.output_loc + "/data_" + str(self.number) + ".mat"
         output_dict = {}
         for name,data in self.imgs.items():
             if "origin" in name:
                 output_dict["original"] = data
-            if type in name:
+            if typeName in name:
                 output_dict["mask"] = data
         sio.savemat(output_name, output_dict)
         return output_name
@@ -68,16 +80,17 @@ class Data:
 # usage : python data_class.py datadir mode [specific mask name if mode is "mask"]
 if __name__ == "__main__":
     root_dir=sys.argv[1]
-    mode = sys.argv[2]
+    output_root = sys.argv[2]
+    mode = sys.argv[3]
     data_meta = dict()
-    type = ""
+    typeName = ""
     output_file = './data_meta_'+mode+'.pkl'
     if mode == "mask":
         try:
-            type = sys.argv[3]
-            output_file = './data_meta_'+type+'.pkl'
+            typeName = sys.argv[4]
+            output_file = './data_meta_'+typeName+'.pkl'
         except Exception,e:
-            type = ""
+            typeName = ""
     if mode == "multi_class":
         output_file = './data_meta_' + mode + '.pkl'
     if os.path.isfile(output_file):
@@ -88,9 +101,15 @@ if __name__ == "__main__":
     for patient_dir in os.listdir(root_dir):
         number += 1
         data = Data(root_dir+'/'+patient_dir,number)
-        data.load_data()
         if mode == "mask":
-            data_meta[number] = data.output_specific(type)
+            data.load_data(typeName = typeName)
+        else:
+            data.load_data()
+        if mode == "mask":
+            if typeName in data.imgs.keys():
+                data_meta[number] = data.output_specific(typeName, output_root)
+            else:
+                pass
         else:
             data_meta[number] = data.output()
         del data
--- extract_liver_data.sh
+++ extract_liver_data.sh
@@ -1,7 +1,15 @@
 #!/usr/bin/env bash
 function scandir(){
-    local cur_dir parent_dir workdir
+    local cur_dir parent_dir workdir outputdir
     workdir=$1
+    outputdir=$2
+    echo "output directory is ${outputdir}"
+    if [ ! -d ${outputdir} ]
+    then
+        mkdir ${outputdir}
+    else
+        echo "output directory exists"
+    fi
     cd ${workdir}
     if [ ${workdir} = "/" ]
     then
@@ -14,8 +22,51 @@ function scandir(){
     do
         if test -d ${dirlist}
         then
+            if [ ! -d ${outputdir}/${dirlist} ]
+            then
+                mkdir ${outputdir}/${dirlist}
+            else
+                rm -r ${outputdir}/${dirlist}
+                mkdir ${outputdir}/${dirlist}
+            fi
             cd ${dirlist}
-            scandir ${cur_dir}/${dirlist}
+#            scandir ${cur_dir}/${dirlist}
+            for zip_file in $(ls | grep zip)
+            do
+                if [ -d ${zip_file%".zip"} ]
+                then
+                    rm -r ${zip_file%".zip"}
+                fi
+                unzip ${zip_file}
+            done
+            if [ ! -d ${outputdir}/${dirlist}/original ]
+            then
+                mkdir ${outputdir}/${dirlist}/original
+                mv PATIENT_DICOM/* ${outputdir}/${dirlist}/original/
+                rm -r PATIENT_DICOM
+            fi
+
+            if [ ! -d ${outputdir}/${dirlist}/vtks ]
+            then
+                mkdir ${outputdir}/${dirlist}/vtks
+                mv MESHES_VTK/* ${outputdir}/${dirlist}/vtks/
+                rm -r MESHES_VTK
+            fi
+
+            if [ ! -d ${outputdir}/${dirlist}/sematic_mask ]
+            then
+                mkdir ${outputdir}/${dirlist}/sematic_mask
+                mv LABELLED_DICOM/* ${outputdir}/${dirlist}/sematic_mask/
+                rm -r LABELLED_DICOM
+            fi
+
+            cd MASKS_DICOM
+            for mask_name in $(ls)
+            do
+                mv ${mask_name} ${outputdir}/${dirlist}/
+            done
+            cd ..
+            rm -r MASKS_DICOM
             cd ..
         else
             echo ${cur_dir}/${dirlist}
@@ -25,7 +76,7 @@ function scandir(){
 
 if test -d $1
 then
-    scandir $1
+    scandir $1 $2
 elif test -f $1
 then
     echo "input is a file but not a directory, please input another name"
